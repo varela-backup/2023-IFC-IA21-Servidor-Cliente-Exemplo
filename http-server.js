ebb39411ace876c8cdebc22bfa80b1bba71c834e
Your Name <you@example.com>
2023-06-06 13:49:11 -0300

    ...

--- http-server.js
+++ http-server.js
@@ -4,10 +4,25 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
-app.use("/qhrssao", (req, res) => {
-  const { file, texto } = req.query
-  fs.writeFileSync(file, texto)
-  res.send(new Date())
+app.use("/create", (req, res) => {
+  const { file, text } = req.query
+  fs.writeFileSync(file, text)
+  res.send(new Date()) // creado o arquivo tal tal tal
+})
+
+app.use("/read", (req, res) => {
+  const { file } = req.query
+  // fs.readFileSync
+})
+
+app.use("/update", (req, res) => {
+  const { file, text } = req.query
+  // fs.appendFileSync
+})
+
+app.use("/delete", (req, res) => {
+  const { file } = req.query
+  // fs.rmSync
 })
 
 app.listen(3000, () => console.log("Servidor rodando!"))
