69e15124ad5746e4b06ce7c2e983a5a8e32873d0
Your Name <you@example.com>
2023-06-13 15:16:34 -0300

    ...

--- src/index.js
+++ src/index.js
@@ -4,15 +4,12 @@ const { getDatabaseInstance } = require("./database")
 
 const app = express()
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/../public'))
 
 app.use("/create", async (req, res) => {
   const { title, source, description, thumb } = req.query
   const db = await getDatabaseInstance()
-  const result = await db.run(`
-    INSERT INTO movies(title, source, description, thumb) VALUES(?, ?, ?, ?)`,
-    [title, source, description, thumb]
-  )
+  const result = await db.run(`INSERT INTO movies(title, source, description, thumb) VALUES(?, ?, ?, ?)`, [title, source, description, thumb])
   res.send(result)
 })
 
