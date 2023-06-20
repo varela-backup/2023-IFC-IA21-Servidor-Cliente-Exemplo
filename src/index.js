b440e612dc6cae8ed588c511f1358d57a429e988
Your Name <you@example.com>
2023-06-20 10:54:04 -0300

    ...

--- src/index.js
+++ src/index.js
@@ -10,22 +10,36 @@ app.use("/create", async (req, res) => {
   const { title, source, description, thumb } = req.query
   const db = await getDatabaseInstance()
   const result = await db.run(`INSERT INTO movies(title, source, description, thumb) VALUES(?, ?, ?, ?)`, [title, source, description, thumb])
-  res.send(result)
+  res.json(result)
 })
 
-app.use("/read", (req, res) => {
-  const { file } = req.query
-  // fs.readFileSync
+app.use("/read", async (req, res) => {
+  const { id } = req.query
+  const db = await getDatabaseInstance()
+  if (id) {
+    const result = await db.get(`SELECT * FROM movies WHERE id=?`, id)
+    res.json(result)
+    return
+  }
+  const result = await db.all(`SELECT * FROM movies`)
+  res.json(result)
 })
 
-app.use("/update", (req, res) => {
-  const { file, text } = req.query
-  // fs.appendFileSync
+app.use("/update", async (req, res) => {
+  const { title, source, description, thumb, id } = req.query
+  const db = await getDatabaseInstance()
+  const result = await db.run(
+    `UPDATE movies SET title=?, source=?, description=?, thumb=? WHERE id=?`,
+    title, source, description, thumb, id
+  )
+  res.json(result)
 })
 
-app.use("/delete", (req, res) => {
-  const { file } = req.query
-  // fs.rmSync
+app.use("/delete", async (req, res) => {
+  const { id } = req.query
+  const db = await getDatabaseInstance()
+  const result = await db.run(`DELETE FROM movies WHERE id=?`, id)
+  res.json(result)
 })
 
 app.listen(3000, () => console.log("Servidor rodando!"))
